b8e1c64fad2b98bf684d4124981af0976023cd5f
Paul Healy <lmiphay@gmail.com>
2015-10-25 16:47:41 +0000

    import classes to package level; add version

---
+++ koam/__init__.py
@@ -0,0 +1,15 @@
+__title__ = 'koam'
+from .version import __version__
+__author__ = 'Paul Healy'
+__license__ = 'GPL-2'
+__copyright__ = 'Copyright 2015, Paul Healy under GPL-2'
+
+from koam.controller import KoamController
+from koam.font import KoamFont
+from koam.host import KoamHost
+from koam.mainwindow import KoamMainWindow
+from koam.process import KoamProcess
+from koam.status import KoamStatus
+from koam.text import KoamText
+from koam.toolbar import KoamToolbar
+from koam.view import KoamView
