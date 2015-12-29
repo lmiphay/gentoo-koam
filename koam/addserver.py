c20aca3ef4f849b36eef66da85fb9b3276444cb6
Paul Healy <lmiphay@gmail.com>
2015-12-29 18:33:05 +0000

    add logging of servers to be added

--- koam/addserver.py
+++ koam/addserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import logging
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import koam
@@ -8,10 +9,13 @@ class KoamAddServer(QLineEdit):
 
     def __init__(self, parent=None):
         QLineEdit.__init__(self, parent)
+        self.logger = logging.getLogger("koam.add.server")
         self.setPlaceholderText('Enter new server name(s) (space separated hostnames)')
         self.returnPressed.connect(self.add)
 
     def add(self):
-        for server in self.text().split():
+        self.logger.log(logging.INFO, "adding server(s): %s", self.text())
+        for server in str(self.text()).split():
+            self.logger.log(logging.INFO, "adding server: %s", server)
             koam.KoamObserver.add_server(server)
         self.clear()
