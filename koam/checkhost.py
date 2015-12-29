a67ee8cf547cb65edc77e27152b98ab7d8089176
Paul Healy <lmiphay@gmail.com>
2015-12-29 18:36:25 +0000

    working version

--- koam/checkhost.py
+++ koam/checkhost.py
@@ -15,25 +15,25 @@ class KoamCheckHost(QObject):
     def __init__(self, controller):
         QObject.__init__(self)
         self.controller = controller
-        self.logger = logging.getLogger("koam.check.host." + server)
+        self.logger = logging.getLogger("koam.check.host")
         koam.KoamObserver.connect_add(self.add)
         self.runner = {}
 
     def add(self, server):
-        self.logger.log(logging.INFO, "checking %s", server)
+        self.logger.log(logging.INFO, "check host %s", server)
         self.runner[server] = koam.KoamCommand(server, 'ssh',
-                                               [koam.DISABLE_PASS_QUERY.DISABLE_PASS_QUERY, server,
-                                                'oam-checkconfig', '-e'])
+                                               koam.KoamController.DISABLE_PASS_QUERY +
+                                               [str(server), 'oam-checkconfig', '-e'])
         self.runner[server].result.connect(self.fin)
         self.runner[server].run()
         self.logger.log(logging.INFO, "check started for %s", server)
 
     def fin(self, server, exitCode):
         if exitCode == 0:
-            self.logger.log(logging.INFO, "check passed: result for %s = %d", server, exitCode)
-            self.controller.add(self.server)
+            self.logger.log(logging.INFO, "check passed: %s had %d failures", server, exitCode)
+            self.controller.add(server)
         else:
-            self.logger.log(logging.ERROR, "check failed: result for %s = %d", server, exitCode)
-            self.controller.err(self.server, 'check host failed' + str(self.runner[server].output))
+            self.logger.log(logging.ERROR, "check failed: %s had %d failures", server, exitCode)
+            self.controller.err(server, 'check host failed' + str(self.runner[server].output))
         del self.runner[server]
         self.logger.log(logging.INFO, "check complete for %s", server)
